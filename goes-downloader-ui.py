69add65c7c6fbecb786e10d64975aaab3a276e67
Douglas Uba <dougmessias@gmail.com>
2020-05-06 15:39:21 -0300

    Add icon

--- goes-downloader-ui.py
+++ goes-downloader-ui.py
@@ -4,11 +4,13 @@ __author__ = 'Douglas Uba'
 __email__  = 'douglas.uba@inpe.br'
 
 import goes.widgets
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 import sys
 
 app = QApplication(sys.argv)
 app.setStyle('Fusion')
 w = goes.widgets.Downloader()
+w.setWindowIcon(QIcon('./resources/icon.png'))
 w.show()
 sys.exit(app.exec_())
